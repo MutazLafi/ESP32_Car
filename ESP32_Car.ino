285b58162a12732865150829a48f6c44ba538801
Mutaz Lafi <mutazhp@hotmail.com>
2024-03-16 15:04:45 +0300

    include Libraries and Add Part of the definitions

--- ESP32_Car.ino
+++ ESP32_Car.ino
@@ -1,3 +1,23 @@
+/*
+ESP32 Car By Mutaz Lafi 
+Written in 2024
+*/
+
+//Libraries
+
+//Connectivity and Wifi Libraries
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+
+// Camera Library
+#include "OV2640"
+
+//definitions 
+#define Flash_Pin 4
+#define SteeringServo_Pin 13
+#define RotatingServo_Pin 12
+
 void setup() {
   // put your setup code here, to run once:
 }
